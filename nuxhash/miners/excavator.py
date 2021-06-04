b9c7c925242ed13ddf76cba58f8ce4778fd536b7
bunyjan <83098336+bunyjan@users.noreply.github.com>
2021-06-05 02:37:03 +0700

    Update excavator.py

--- nuxhash/miners/excavator.py
+++ nuxhash/miners/excavator.py
@@ -10,10 +10,10 @@ from nuxhash.utils import get_port
 
 
 ALGORITHMS = [
-    'equihash',
+    #'equihash',
     #'pascal',
     #'decred',
-    #'KAWPOW',
+    'kapow',
     #'lbry',
     #'blake2s',
     #'daggerhashimoto',
